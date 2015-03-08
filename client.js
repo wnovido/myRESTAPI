f49c4328a844c1ec104fe3b38b06262c4bf84c76
wilso_000 <wilson_novido@yahoo.com>
2015-03-08 07:11:39 -0400

    Added Update/Delete and Get 1 product

--- client.js
+++ client.js
@@ -39,3 +39,84 @@ client.get('/products', function (err, req, res, products) {
         console.log(products);
     }
 });
+
+testProduct.price = "1000 USD",
+client.put('/product/' + testProduct.id, testProduct, function (err, req, res, status) {
+    if (err) {
+        console.log("An error ocurred >>>>>>");
+        console.log(err);
+    } else {
+        console.log('Product saved >>>>>>>');
+        console.log(status);
+    }
+ 
+});
+
+
+client.del('/product/' + testProduct.id, function (err, req, res, status) {
+    if (err) {
+        console.log("An error ocurred >>>>>>");
+        console.log(err);
+    } else {
+        console.log('Product deleted >>>>>>>');
+        console.log(status);
+    }
+});
+
+
+client.get('/product/' + testProduct.id, function (err, req, res, product) {
+    if (err) {
+        console.log("An error ocurred >>>>>>");
+        console.log(err);
+    } else {
+        console.log('Product with id ' + product.id + '  >>>>>>>');
+        console.log(product);
+    }
+});
+
+
+// to see the output when you the run the client, A nested async callback system to do the above steps
+console.log("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> \n");
+client.get('/products', function (err, req, res, products) {
+    if (err) console.log("Oops : ", err);
+    else console.log('Total products : ', products.length);
+    console.log("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> \n");
+ 
+    client.post('/product', testProduct, function (err, req, res, prod) {
+        if (err) console.log("Oops : ", err);
+        else console.log('Inserted product : ', prod);
+        console.log("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> \n");
+ 
+        client.get('/product/' + testProduct.id, function (err, req, res, prod) {
+            if (err) console.log("Oops : ", err);
+            else console.log('Product with ID :' + testProduct.id + ' :: ', prod);
+            console.log("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> \n");
+ 
+            client.put('/product/' + testProduct.id, {
+                price: "999 USD"
+            }, function (err, req, res, status) {
+                if (err) console.log("Oops : ", err);
+                else console.log('Product Updated status :', status);
+                console.log("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> \n");
+ 
+                client.get('/product/' + testProduct.id, function (err, req, res, prod) {
+                    if (err) console.log("Oops : ", err);
+                    else console.log('Product with ID :' + testProduct.id + ' :: ', prod);
+                    console.log("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> \n");
+ 
+                    client.del('/product/' + testProduct.id, function (err, req, res, status) {
+                        if (err) console.log("Oops : ", err);
+                        else console.log('Product deleted status :', status);
+                        console.log("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> \n");
+                        client.get('/products', function (err, req, res, products) {
+                            if (err) console.log("Oops : ", err);
+                            else console.log('Total products : ', products.length);
+                            console.log("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> \n");  
+                        });
+ 
+                    });
+                });
+            });
+        });
+    });
+});
--- server.js
+++ server.js
@@ -40,6 +40,66 @@ server.post('/product', function (req, res, next) {
 });
 
 
+
+server.put('/product/:id', function (req, res, next) {
+    // get the existing product
+    db.products.findOne({
+        id: req.params.id
+    }, function (err, data) {
+        // merge req.params/product with the server/product
+ 
+        var updProd = {}; // updated products 
+        // logic similar to jQuery.extend(); to merge 2 objects.
+        for (var n in data) {
+            updProd[n] = data[n];
+        }
+        for (var n in req.params) {
+            updProd[n] = req.params[n];
+        }
+        db.products.update({
+            id: req.params.id
+        }, updProd, {
+            multi: false
+        }, function (err, data) {
+            res.writeHead(200, {
+                'Content-Type': 'application/json; charset=utf-8'
+            });
+            res.end(JSON.stringify(data));
+        });
+    });
+    return next();
+});
+
+
+
+server.del('/product/:id', function (req, res, next) {
+    db.products.remove({
+        id: req.params.id
+    }, function (err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'application/json; charset=utf-8'
+        });
+        res.end(JSON.stringify(true));
+    });
+    return next();
+});
+
+
+
+
+server.get('/product/:id', function (req, res, next) {
+    db.products.findOne({
+        id: req.params.id
+    }, function (err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'application/json; charset=utf-8'
+        });
+        res.end(JSON.stringify(data));
+    });
+    return next();
+});
+
+
 module.exports = server;
 
 
